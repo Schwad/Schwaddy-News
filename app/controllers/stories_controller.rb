2e1b906881e46d9d4e9658bf0b116a5a2aa4b8a4
Nick Schwaderer <nicholas.schwaderer@gmail.com>
2015-07-13 12:40:21 -0600

    incorporates scraping and rendering of reddit webdev page

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,5 +1,17 @@
 class StoriesController < ApplicationController
   def index
-    @stories = Story.all
+
+    @y_stories = Story.where("originplace = 'Hacker News'")
+    @r_stories = Story.where("originplace = 'Reddit Webdev'")
+    iterator_one = 0
+    iterator_two = 0
+    @stories = []
+    while @y_stories.length > iterator_two && @r_stories.length > iterator_two
+      @stories[iterator_one] = @y_stories[iterator_two] if @y_stories.length > iterator_two
+      iterator_one += 1
+      @stories[iterator_one] = @r_stories[iterator_two] if @r_stories.length > iterator_two
+      iterator_one += 1
+      iterator_two += 1
+    end
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
   def updates_page
     remove_all
     update_ycombinator
-    # update_reddit
+    update_reddit
     # update_longform
     # update_thebrowser
     # update_slashdot
@@ -65,6 +65,47 @@ module ApplicationHelper
     end
   end
 
+  def update_reddit
+    puts "Updating reddit...."
+    @a = Mechanize.new
+    @a = @a.get('https://www.reddit.com/r/webdev')
+    get_reddit_stories_and_links
+    get_reddit_scores
+    iterator = 0
+    @new_story = true
+    puts "Creating stories..."
+    while iterator < @scores.length
+      if @old_stories.include?(@y_stories[0][iterator])
+        @new_story = false
+      else
+        @new_story = true
+      end
+      Story.create(
+          :source => @reddit_stories[0][iterator],
+          :href => @reddit_stories[1][iterator],
+          :points_text => @reddit_scores[iterator],
+          :originplace => 'Reddit Webdev',
+          :is_new => @new_story
+        )
+      iterator += 1
+    end
+    puts "Reddit updated!"
+  end
+
+  def get_reddit_stories_and_links
+    @reddit_stories = [[],[]]
+    @a.search('a.title.may-blank').each do |article|
+      @reddit_stories[0] << article.text
+      @reddit_stories[1] << article.to_s.scan(/href="(.*)\/"/)[0]
+    end
+  end
+
+  def get_reddit_scores
+    @reddit_scores = []
+    @a.search('div.score.unvoted').each do |score|
+      @reddit_scores << "#{score.text} points"
+    end
+  end
 
 
 end
